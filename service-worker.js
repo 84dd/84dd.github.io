e4070085fe6e7e3549c2c6ea190da44f74fdc1d1
lanyongmao <744177242@qq.com>
2019-09-07 16:35:51 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -21,27 +21,27 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "3490d0c4f3c98665d6a9cd1319b473de"
+    "revision": "ce23980bc17cd4bf854573aa969a8798"
   },
   {
     "url": "about/index.html",
-    "revision": "e48232d399b8cd0fa5fdfbae78a38733"
+    "revision": "00a80eded387813746eec6c6bde52a23"
   },
   {
-    "url": "assets/css/0.styles.802853a7.css",
-    "revision": "4314a0a5f27499ddcbd36d5c43cff2bc"
+    "url": "assets/css/0.styles.c3c29ef4.css",
+    "revision": "69f7691ff04dc0c05359cdc48864cb61"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.d9c8e4bf.js",
-    "revision": "bcdc5ab3956c76b081cf0af363989497"
+    "url": "assets/js/10.c2178157.js",
+    "revision": "2977f5856137a7a5e168773b75ef00b5"
   },
   {
-    "url": "assets/js/11.28fbd523.js",
-    "revision": "f1789cf5d0d6c7d4fd7fb3a0062db2e5"
+    "url": "assets/js/11.7b870ddc.js",
+    "revision": "71a519533aff103f5f28ba93692dd13e"
   },
   {
     "url": "assets/js/12.f5bb1f1a.js",
@@ -76,8 +76,8 @@ self.__precacheManifest = [
     "revision": "d075e660a1bf1407d89fda6ca547d6b5"
   },
   {
-    "url": "assets/js/app.0e6fa8ec.js",
-    "revision": "341c0320f49a4bb3397b7e7da11079bd"
+    "url": "assets/js/app.75f4d12d.js",
+    "revision": "c51346a301c0091868ae38dccd6aa52b"
   },
   {
     "url": "assets/js/vendors~docsearch.05d8cabe.js",
@@ -85,7 +85,7 @@ self.__precacheManifest = [
   },
   {
     "url": "docs/en/index.html",
-    "revision": "4e10d2ce9b3cd6df80190e767e7cb932"
+    "revision": "56e7b781ba0157c12ad85310208f2768"
   },
   {
     "url": "fish_logo.png",
@@ -93,15 +93,15 @@ self.__precacheManifest = [
   },
   {
     "url": "guide/index.html",
-    "revision": "9a03eabb4d8e804da3c8febe78a1e9e7"
+    "revision": "cd2ce1d2ce0fbb669a3701dd7a95a034"
   },
   {
     "url": "index.html",
-    "revision": "a8b78e02916085f735f4a6fced9194a9"
+    "revision": "d203f82e0c9088ccafe608dff1e81356"
   },
   {
     "url": "passages/2019-09-01-browser/index.html",
-    "revision": "03c8e96a040320290fd3206f4cb2c611"
+    "revision": "833337efdf64710b99414db94b768848"
   },
   {
     "url": "qq.jpg",
