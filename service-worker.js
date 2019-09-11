ad4381da3a84f4e718592be7ddb85dd09d834805
lanyongmao <744177242@qq.com>
2019-09-11 23:36:52 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -21,71 +21,95 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "7d9b19221d25c7689ba77c5f5237849c"
+    "revision": "d3d4bb23964f0a45b874fbe550877228"
   },
   {
     "url": "about/index.html",
-    "revision": "fdefb8f4e7c1d1e0ec0e7c47f98aae27"
+    "revision": "03564d5c37551c63ae4742f569e8e1ca"
   },
   {
-    "url": "assets/css/0.styles.4e21bb6b.css",
-    "revision": "a1fc4899f4590a5aea07d521356dd1f8"
+    "url": "assets/css/0.styles.bf87837d.css",
+    "revision": "ab201bc9b2c6172c1c2adab7876170c7"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.1fa1acbe.js",
-    "revision": "10ad676eb7ad2766ecb9099627f3045b"
+    "url": "assets/js/10.ca6c864a.js",
+    "revision": "45e7eff2d8570febc6e6120a8a46b3d4"
   },
   {
-    "url": "assets/js/11.d2fc0d0c.js",
-    "revision": "aa64d5150f055960b605973920858651"
+    "url": "assets/js/11.cca1d4b5.js",
+    "revision": "992398084d3548703b102695680c567f"
   },
   {
-    "url": "assets/js/12.a9ab67d0.js",
-    "revision": "f2edd319957a75051073c35bb0628a3b"
+    "url": "assets/js/12.7d0309ab.js",
+    "revision": "d285a4e719d73129175e94a6ee5b6bf7"
   },
   {
-    "url": "assets/js/3.2c23821a.js",
-    "revision": "7ea90385c7751111f6e200bc5984d2ac"
+    "url": "assets/js/13.7cc5de8c.js",
+    "revision": "460972f8d5d228542831e5a532aea895"
   },
   {
-    "url": "assets/js/4.352ade53.js",
-    "revision": "c70626578e1b187a36b9a3c6e4695b09"
+    "url": "assets/js/14.fd616c98.js",
+    "revision": "fd2cfbf19ef24a434f19b7e0366b0856"
   },
   {
-    "url": "assets/js/5.5e6e16a0.js",
-    "revision": "e6d7e2bada2dd30ae01e530d149f532f"
+    "url": "assets/js/2.3a598e34.js",
+    "revision": "f42cbbd2c5be9bae2c1457990b06fb74"
   },
   {
-    "url": "assets/js/6.a874133b.js",
-    "revision": "3db41d276e2577f1a38809ea0052c104"
+    "url": "assets/js/3.2023c5a7.js",
+    "revision": "5ca37cffbfcab2d132b083fdfea0a664"
   },
   {
-    "url": "assets/js/7.3b383147.js",
-    "revision": "b48ee5fe0cd40a02a8bdf2baad5de223"
+    "url": "assets/js/4.66e979b8.js",
+    "revision": "4719e8e83e4852fd3b1f07e7b363ff44"
   },
   {
-    "url": "assets/js/8.27152d61.js",
-    "revision": "e96471833071e22a735a9b0b913ccb28"
+    "url": "assets/js/5.b057fba3.js",
+    "revision": "dbb5d61f4603928cccc826b577bd0119"
   },
   {
-    "url": "assets/js/9.f5c42836.js",
-    "revision": "e396e6529fe07df4d379d6c9f9a01586"
+    "url": "assets/js/6.e35234c1.js",
+    "revision": "a79b6c71bc8c19f59e10ee42ced5362b"
   },
   {
-    "url": "assets/js/app.212c27c5.js",
-    "revision": "d169b815932b21068aa4283cd4f69a22"
+    "url": "assets/js/7.fb928adc.js",
+    "revision": "ecece08a5f6e8b11f0ab15c8e348112f"
   },
   {
-    "url": "assets/js/vendors~docsearch.10c30797.js",
-    "revision": "3ce6078b59477c3b34ad8f37632dc7fe"
+    "url": "assets/js/8.861600fb.js",
+    "revision": "6a261483c74fda9be100cfd824d092b6"
+  },
+  {
+    "url": "assets/js/9.916be0bc.js",
+    "revision": "2232cddee1808d544bfe275cdc530c17"
+  },
+  {
+    "url": "assets/js/app.8e2bd645.js",
+    "revision": "f731eb37070df7faf3fd577ed6ab7ecd"
+  },
+  {
+    "url": "docs/chrome/index.html",
+    "revision": "fab4390b8e5e093e92f00bb0074377c1"
   },
   {
     "url": "docs/en/index.html",
-    "revision": "ad087600b6b5934d93bc1be8cdeb21e4"
+    "revision": "76a202e47b369b44d465a75f1bc0733e"
+  },
+  {
+    "url": "docs/idea/index.html",
+    "revision": "f45b22ce983e08c145c7b49b3ae8ccec"
+  },
+  {
+    "url": "docs/idea/技巧.html",
+    "revision": "2c38f67ce816cc732b4a3470e61c8731"
+  },
+  {
+    "url": "docs/idea/插件.html",
+    "revision": "6ea2b381bc9d38a7c871626904bc3853"
   },
   {
     "url": "fish_logo.png",
@@ -93,15 +117,19 @@ self.__precacheManifest = [
   },
   {
     "url": "guide/index.html",
-    "revision": "8aa6f8fdccbce5e52619a47e92113be1"
+    "revision": "6f805f0522188c14544851eaf11ccfb4"
+  },
+  {
+    "url": "idea/BackgroundImagePlus.png",
+    "revision": "6a98c488409a1d1ae181e98db85a2038"
+  },
+  {
+    "url": "idea/NyanProgressBar.png",
+    "revision": "a855ca398e2cd4d3b5aefe2bb887f7bb"
   },
   {
     "url": "index.html",
-    "revision": "a11b61a6fe54f98c3b3c8d03096520fe"
-  },
-  {
-    "url": "passages/tool/chrome/index.html",
-    "revision": "0a665a758db284a6b9338887989e8bd3"
+    "revision": "a4f26072ae44ddb22d364eb8728fa279"
   },
   {
     "url": "qq.jpg",
