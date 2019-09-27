047e61601b04fb02090bbf8a9ae08cc61d74e4ef
lanyongmao <744177242@qq.com>
2019-09-27 18:13:27 +0800

    增加一键部署到gitee

--- deploy.js
+++ deploy.js
@@ -4,5 +4,12 @@ ghpages.publish('dist', {
   branch: 'master',
   repo: 'https://github.com/84dd/84dd.github.io.git'
 }, () => {
-  console.log('Publish done!')
+  console.log('Github publish done!')
+});
+
+ghpages.publish('dist', {
+  branch: 'master',
+  repo: 'https://gitee.com/84dd/84dd.git'
+}, () => {
+  console.log('Gitee publish done!')
 });
